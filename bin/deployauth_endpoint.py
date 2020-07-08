e80d2a672d1688014fc001fcb1643457528a5482
Will Searle <w.searle@nhs.net>
2020-07-08 23:16:55 +0100

    Removed unusused imports

--- bin/deployauth_endpoint.py
+++ bin/deployauth_endpoint.py
@@ -1,59 +1,37 @@
-from urlparse import urlparse
-
-import lxml.etree as et
 import logging
-import urllib
-import time
 import json
-import base64
-import cStringIO
-import re
 import os, subprocess, sys
-import splunk
 import splunk.rest as rest
 import splunk.entity as entity
-import splunk.auth
-import splunk.models.dashboard as sm_dashboard
-import splunk.models.dashboard_panel as sm_dashboard_panel
-import splunk.models.saved_search as sm_saved_search
-import splunk.search
-import splunk.search.searchUtils
 from splunk.util import normalizeBoolean, readSplunkFile
-from pprint import pformat
-
 from splunk.persistconn.application import PersistentServerConnectionApplication
 
 logger = logging.getLogger('splunk.rest')
 splunk_home     = os.path.normpath(os.environ["SPLUNK_HOME"])
 isWindows  = ("win32" == sys.platform)
 
+#Borrowed from splunk.cli.cli_common
 def decrypt(value):
-  """Decrypts encrypted conf values, e.g. sslPassword.
-     Encrypted values start with $1$ (RC4) or $7$ (AES-GCM)"""
-  launcher_path = os.path.join(splunk_home, "bin", "splunk")
-  if isWindows:
-    launcher_path += ".exe"
-  # show-decrypted CLI command added in 7.2.x
-  cmd = [launcher_path, 'show-decrypted', '--value', value]
-  p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  out, err = p.communicate()
-  if sys.version_info >= (3, 0):
-    out = out.decode()
-    err = err.decode()
-  # p.returncode is always 0 so check stderr
-  if err:
-    logger.error(
-        'Failed to decrypt value: {}, error: {}'.format(value, err))
-    return None
-  return out.strip()
+    """Decrypts encrypted conf values, e.g. sslPassword.
+       Encrypted values start with $1$ (RC4) or $7$ (AES-GCM)"""
+    launcher_path = os.path.join(splunk_home, "bin", "splunk")
+    if isWindows:
+        launcher_path += ".exe"
+    # show-decrypted CLI command added in 7.2.x
+    cmd = [launcher_path, 'show-decrypted', '--value', value]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if sys.version_info >= (3, 0):
+        out = out.decode()
+        err = err.decode()
+    # p.returncode is always 0 so check stderr
+    if err:
+        logger.error(
+            'Failed to decrypt value: {}, error: {}'.format(value, err))
+        return None
+    return out.strip()
 
 def flatten_query_params(params):
-    # Query parameters are provided as a list of pairs and can be repeated, e.g.:
-    #
-    #   "query": [ ["arg1","val1"], ["arg2", "val2"], ["arg1", val2"] ]
-    #
-    # This function simply accepts only the first parameter and discards duplicates and is not intended to provide an
-    # example of advanced argument handling.
     flattened = {}
     for i, j in params:
         flattened[i] = flattened.get(i) or j
@@ -91,4 +69,4 @@ class handler(PersistentServerConnectionApplication):
             logger.warning(app_resp)
             return {'payload': app_download,
                     'status': 200          # HTTP status code
-            }
+                    }
